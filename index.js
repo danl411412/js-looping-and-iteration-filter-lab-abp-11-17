e07925f7a5b83f1e71bc64f8c8f8e593440735b1
danl411412 <daan.b.lee@gmail.com>
2017-11-23 00:56:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(arr, name) {
     return arr.filter(function(x) {
-      return x === name;
+      return x.toLowerCase() === name.toLowerCase();
     })
 }
